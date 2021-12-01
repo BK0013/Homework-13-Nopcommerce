094d40851ffda3cd821b852807317e46e54a33c3
Bagajera13 <bharatgajera.8@gmail.com>
2021-12-01 00:16:48 +0000

    xcart homework week13

---
+++ src/main/java/com/nopcommerce/demo/pages/HomePage.java
@@ -0,0 +1,24 @@
+package com.nopcommerce.demo.pages;
+
+public class HomePage {
+    public void clickOnGiftCardsLink() {
+    }
+
+    public void clickOnComputerLink() {
+    }
+
+    public void clickOnDigitalDownloadsLink() {
+    }
+
+    public void clickOnJewleryLink() {
+    }
+
+    public void clickOnElectronicsLink() {
+    }
+
+    public void clickOnBooksLink() {
+    }
+
+    public void clickOnApparelLink() {
+    }
+}
